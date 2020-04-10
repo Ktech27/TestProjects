818069b7a1a78d0c6fa7f7163b2004a887d44350
Katherine Cardona <kalexa2707@gmail.com>
2020-04-09 22:36:46 -0400

    Wikipedia webtest startbrowser,test&close with selenium

---
+++ WikiTest/WikiTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WikiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       
+            WebTest wTest = new WebTest();
+            wTest.startBrowser();
+            wTest.makeTest();
+            wTest.closeBrowser();
+        }
+    }
+}
---
+++ WikiTest/WikiTest/WebTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace WikiTest
+{
+    class WebTest
+    {
+        IWebDriver webDriver; // variable of type IWebDriver
+
+        public void startBrowser()    // Initial line void is not return information
+        {
+            webDriver = new ChromeDriver(@"C:\Users\Katherin\Documents\Testing");   
+        }
+
+        public void makeTest()
+
+         {
+            webDriver.Url = "https://www.wikipedia.org/";
+            //webDriver.FindElement(By.XPath("/html/body/div[3]/form/fieldset/div/input")).SendKeys("COVID19" + Keys.Enter);
+            webDriver.FindElement(By.Id("searchInput")).SendKeys("COVID19"+Keys.Enter);  
+
+
+
+
+        }
+
+        public void closeBrowser()
+
+        {
+            webDriver.Close();
+        
+        }
+
+
+
+
+    }
+}

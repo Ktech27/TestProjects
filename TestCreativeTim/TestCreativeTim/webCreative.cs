d9f3f440cc67c6e26809bbb1174ba0f32409ae5c
Ktech27 <alexaqa21@outlook.com>
2020-04-14 19:58:03 -0400

    Working in CreativeTim web site

--- TestCreativeTim/TestCreativeTim/webCreative.cs
+++ TestCreativeTim/TestCreativeTim/webCreative.cs
@@ -37,13 +37,42 @@ namespace TestCreativeTim
 
             // Click Dashboard back return 
 
-
-
-
             //Driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/nav/div/div[2]/ul/li[1]/a")).Click();
             Driver.Navigate().Back();
-        
-        
+
+
+            // Third Element click
+            IWebElement Listview2 = Driver.FindElement(By.ClassName("caret"));
+            Listview2.Click();
+            System.Threading.Thread.Sleep(500);
+            Driver.FindElement(By.CssSelector("div.content div.wrapper div.sidebar div.sidebar-wrapper.text-left ul.nav div:nth-child(2) ul.nav li:nth-child(2) a.nav-link > span.sidebar-normal")).Click();
+            System.Threading.Thread.Sleep(500);
+
+            // Scroll Down
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            //IWebElement element3 = Driver.FindElement(By.LinkText(""))
+            // // Select element in this page
+            IWebElement swich2 = Driver.FindElement(By.CssSelector("#app > div > div.wrapper > div.main-panel > div > div > div.col-lg-6.col-sm-6.text-center > div.card.card-contributions > div.card-footer.text-left > div > div:nth-child(2) > div > div > div > span.bootstrap-switch-handle-on"));
+            js.ExecuteScript("arguments[0].scrollIntoView();", swich2);
+            
+            System.Threading.Thread.Sleep(500);
+
+            IWebElement foo = Driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/div[4]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div/div/span[2]"));
+            foo.Click();
+            //swich2.Click();
+            
+            //IebElement swicth = Driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/div[4]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div/div/span[3]"));
+            //swicth.Click();
+
+            
+
+
+            // Driver Return++
+            //Driver.Navigate().Back();
+
+          
+
+
         }
 
     }

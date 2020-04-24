e936de367b146aef835082a0e4bc22e3a6ac2c21
Ktech27 <alexaqa21@outlook.com>
2020-04-23 22:07:27 -0400

    Add For Statement

--- TestProjectAndy/TestProjectAndy/Automatic.cs
+++ TestProjectAndy/TestProjectAndy/Automatic.cs
@@ -13,45 +13,63 @@ namespace TestProjectAndy
     class Automatic
     {
         IWebDriver driver;
-
+        int IDGenerator = 140;
+        string SKUNumber = "SKU: ";
 
         public void Start()
         {
-
+            // Start the browser
             driver = new ChromeDriver(@"C:\Users\Katherin\Documents\Testing");
+            // choose the URL we want to test
             driver.Url = "https://pricerwebapp.azurewebsites.net/product.html";
 
         }
 
+        // Test option create product
         public void Createproduct()
         {
-            IWebElement Create = driver.FindElement(By.Id("btnCreate"));
-            IWebElement Productid = driver.FindElement(By.Id("txtProductID"));
-            IWebElement description = driver.FindElement(By.Id("txtDescription"));
-            IWebElement productName = driver.FindElement(By.Id("txtProductName"));
-            IWebElement productURL = driver.FindElement(By.Id("txtURL"));
-            IWebElement price = driver.FindElement(By.Id("txtPriceDollar"));
-            IWebElement SKU = driver.FindElement(By.Id("txtSKU"));
-            IWebElement Pricecents = driver.FindElement(By.Id("txtPriceCents"));
-            IWebElement model = driver.FindElement(By.Id("txtModel"));
-            IWebElement save = driver.FindElement(By.Id("btnCreatProduct"));
+
+            // For statement for sending more than 1 record
+
+            string textToSend = "";
+            for (int i = 0; i <= 5; i++)
+            {
+                IWebElement Create = driver.FindElement(By.Id("btnCreate"));
+                IWebElement Productid = driver.FindElement(By.Id("txtProductID"));
+                IWebElement description = driver.FindElement(By.Id("txtDescription"));
+                IWebElement productName = driver.FindElement(By.Id("txtProductName"));
+                IWebElement productURL = driver.FindElement(By.Id("txtURL"));
+                IWebElement price = driver.FindElement(By.Id("txtPriceDollar"));
+                IWebElement SKU = driver.FindElement(By.Id("txtSKU"));
+                IWebElement Pricecents = driver.FindElement(By.Id("txtPriceCents"));
+                IWebElement model = driver.FindElement(By.Id("txtModel"));
+                IWebElement save = driver.FindElement(By.Id("btnCreatProduct"));
 
 
-            Create.Click();
-            Productid.SendKeys("123");
-            description.SendKeys("Cream to skin");
-            productName.SendKeys("Cetaphil");
-            productURL.SendKeys("www.walmart.ca");
-            price.SendKeys("15");
-            SKU.SendKeys("TEST");
-            Pricecents.SendKeys("56");
-            model.SendKeys("bla");
-            save.Click();
+                Create.Click();
+                textToSend = IDGenerator.ToString();
+                // ID Product We are calling our method IDGenerator
+                Productid.SendKeys(textToSend);
+                description.SendKeys("Cream to skin");
+                productName.SendKeys("Cetaphil");
+                productURL.SendKeys("www.walmart.ca");
+                price.SendKeys("15");
+                // Sending the information in each line
+                textToSend = SKUNumber + IDGenerator.ToString();
+                SKU.SendKeys(textToSend);
+                Pricecents.SendKeys("56");
+                model.SendKeys("bla");
+                save.Click();
+                System.Threading.Thread.Sleep(2000);
 
+                IDGenerator++;
+            }
+
+            System.Threading.Thread.Sleep(2000);
 
             driver.Close();
 
-            
+
 
 
         }
